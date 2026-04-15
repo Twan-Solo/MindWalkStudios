9e07f2cb3127f5540889c1361fbabd9416b3a748
Ethan <e_suter@outlook.com>
2026-04-15 17:00:33 -0400

    Player shoots properly
    Setup the shooting so now the players can shoot at the painted walls.

--- Memoir2/Assets/Scripts/Managers/Player/PlayerAttack.cs
+++ Memoir2/Assets/Scripts/Managers/Player/PlayerAttack.cs
@@ -6,16 +6,25 @@ public class PlayerAttack : MonoBehaviour
 {
     public InputActionReference attack;
     public GameObject bulletPrefab;
-    public Transform bulletSpawnPoint;
-    public float bulletSpeed = 5f;
-    public float rotationSpeed = 10f;
+    public GameObject player;
+    private float playerRotation;
 
+    public float bulletSpawnHeight = 1;
+
+    public void Start()
+    {
+        //assign freshly spawn playerArmature to script
+        player = GameObject.Find("PlayerArmature(Clone)");
+    }
 
     private void Update()
     {
+        //get rotation
+        playerRotation = player.transform.eulerAngles.y;
+        //spawn on attack
         if (attack.action.triggered)
         {
-            Instantiate(bulletPrefab);
+            GameObject bullet = Instantiate(bulletPrefab, transform.position + new Vector3(0, bulletSpawnHeight, 0), Quaternion.Euler(0, playerRotation+90, -90));
         }
     }
 
